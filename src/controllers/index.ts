ef0aea17b4dd769f2b2efc33465435f477f1a1a3
Elithon Silva <elithonsilva@gmail.com>
2022-04-13 19:07:49 -0300

    cria e implementa controller index.ts

---
+++ src/controllers/index.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import GenericService from '../services/GenericService';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+abstract class Controller<T> {
+  abstract route: string;
+
+  protected errors = ControllerErrors;
+
+  constructor(protected service: GenericService<T>) { }
+
+  abstract create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  read = async (
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const cars = await this.service.read();
+      return res.status(201).json(cars);
+    } catch (error) {
+      return res.status(400);
+    }
+  };
+
+  abstract readOne(
+    req: Request<{ id: string }>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+}
+
+export default Controller;
