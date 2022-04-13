c4ed954b5c65799648337c6f0a4302953e5b4253
Elithon Silva <elithonsilva@gmail.com>
2022-04-13 18:55:28 -0300

    implemente CarService

---
+++ src/services/CarService.ts
@@ -0,0 +1,35 @@
+import { Car, carSchema } from '../interfaces/CarInterface';
+import GenericService, { ServiceError } from './GenericService';
+import CarModel from '../models/CarModel';
+
+class CarService extends GenericService<Car> {
+  constructor(model = new CarModel()) {
+    super(model);
+  }
+
+  create = async (obj: Car): Promise<Car | ServiceError> => {
+    const parsed = carSchema.safeParse(obj);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.create(obj);
+  };
+
+  read = async (): Promise<Car[]> => this.model.read();
+
+  readOne = async (id: string): Promise<Car | null | ServiceError> =>
+    this.model.readOne(id);
+
+  update = async (id: string, obj: Car): Promise<Car | null | ServiceError> => {
+    this.model.update(id, obj);
+    return this.model.readOne(id);
+  };
+
+  delete = async (id: string): Promise<Car | null | ServiceError> => {
+    const carMatch = this.model.readOne(id);
+    this.model.delete(id);
+    return carMatch;
+  };
+}
+
+export default CarService;
