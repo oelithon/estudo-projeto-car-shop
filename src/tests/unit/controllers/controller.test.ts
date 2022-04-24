4369b3abbc5548b7d4f1e23ca7e89e71eaa1be44
Elithon Silva <elithonsilva@gmail.com>
2022-04-24 10:01:26 -0300

    implementa teste na camada controller rota /cars

--- src/tests/unit/controllers/controller.test.ts
+++ src/tests/unit/controllers/controller.test.ts
@@ -1,27 +1,55 @@
-// template para criação dos testes de cobertura da camada de controller
+import mongoose from 'mongoose';
+import * as sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp = require('chai-http');
+import server from '../../../server';
+import CarService from '../../../services/CarService';
+import CarController from '../../../controllers/CarController';
 
+const app = server.startServer();
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// import chaiHttp = require('chai-http');
+const carController = new CarController();
+const carService = new CarService();
 
+chai.use(chaiHttp);
 
-// chai.use(chaiHttp);
+const { expect } = chai;
 
-// const { expect } = chai;
+let chaiHttpResponse: Response;
 
-// describe('Sua descrição', () => {
+describe("Testes na camada controller rota '/cars'", () => {
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  describe("", () => {
+    before(async () => {
+      sinon
+        .stub(mongoose.Model, "create")
+        .resolves({
+          model: "Ferrari Maranello",
+          year: 1963,
+          color: "red",
+          buyValue: 3500000,
+          seatsQty: 2,
+          doorsQty: 2
+        });
+    });
 
-//   after(() => {
-//     ().restore();
-//   })
+    after(() => {
+      (mongoose.Model.create as sinon.SinonStub).restore();
+    });
 
-//   it('', async () => { });
+    it('Na camada controller, testa se é criado um novo carro', async () => {
+      let chaiHttpResponse = await chai.request('http://localhost:3001')
+        .post('/cars').send({
+          _id: "4edd40c86762e0fb12000003",
+          model: "Ferrari Maranello",
+          year: 1963,
+          color: "red",
+          buyValue: 3500000,
+          seatsQty: 2,
+          doorsQty: 2
+        });
 
-// });
+      expect(chaiHttpResponse.status).to.be.equal(201);
+    });
+  });
+});
