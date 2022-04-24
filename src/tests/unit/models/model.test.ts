097fadfc6c3c8bd2b8987eb9ebb46f15de129267
Elithon Silva <elithonsilva@gmail.com>
2022-04-24 09:44:45 -0300

    implementa testes na camada model, rota /cars

--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -1,2 +1,41 @@
-// import { expect } from 'chai';
-// import mongoose from 'mongoose';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import CarModel from '../../../models/CarModel';
+
+describe("Testes na camada model rota '/cars'", () => {
+  let carModel = new CarModel();
+
+  describe("", () => {
+    before(() => {
+      sinon.stub(carModel, "create").resolves(
+        {
+          model: "Ferrari Maranello",
+          year: 1963,
+          color: "red",
+          buyValue: 3500000,
+          seatsQty: 2,
+          doorsQty: 2
+        }
+      )
+    });
+
+    after(() => {
+      (carModel.create as sinon.SinonStub).restore();
+    });
+
+    it("Na camada model, testa se é criado um novo carro", async () => {
+      const newCar = {
+        _id: "4edd40c86762e0fb12000003",
+        model: "Ferrari Maranello",
+        year: 1963,
+        color: "red",
+        buyValue: 3500000,
+        seatsQty: 2,
+        doorsQty: 2
+      };
+
+      const car = await carModel.create(newCar);
+      expect(car).to.be.an('object');
+    })
+  });
+});
